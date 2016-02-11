8e58e442b9083d36b2cb9ed99a2ea02b82a06914
davidwessman <david.wesmn@gmail.com>
2016-02-11 21:12:19 +0100

    Update drone

--- drone.py
+++ drone.py
@@ -1,15 +1,31 @@
 import math as math
 class Drone:
     i = 0
-    def __init__(self):
+    def __init__(self, products):
         self.id = Drone.i
         self.r = 0
         self.c = 0
-        self.inventory = []
+        self.free = 0
+        self.inventory = [0] * products
         Drone.i += 1
 
-    def load(self, string):
-        self.inventory = []
+    def load(self, warehouse, product_id, number_of_product):
+        distance = 0
+        distance += self.move(warehouse.r, warehouse.c)
+        self.inventory[product_id] += number_of_products
+        free += distance + 1
+        print(self.id, 'L', warehouse.id, product_id, number_of_product)
+
+    def deliver(self, r, c, order_id, product_id, number_of_product):
+        distance = 0
+        distance += self.move(r, c)
+        self.inventory[product_id] -= number_of_product
+        self.free += distance + 1
+        print(self.id, 'D', warehouse.id, product_id, number_of_product)
+
+    def wait(self, wait):
+        self.free += wait
+        print(self.id, 'W', wait)
 
     def move(self, r, c):
         distance = self.distance(r, c)
