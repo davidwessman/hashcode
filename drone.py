b6eaccca711ea28212117b63223b01dbfe0c13e2
davidwessman <david.wesmn@gmail.com>
2016-02-11 19:53:32 +0100

    Drone

---
+++ drone.py
@@ -0,0 +1,23 @@
+import numpy as num
+import math as math
+class Drone:
+    i = 0
+    def __init__(self):
+        self.id = Drone.i
+        self.r = 0
+        self.c = 0
+        self.inventory = []
+        Drone.i += 1
+
+    def load(self, string):
+        self.inventory = []
+
+    def move(self, r, c):
+        distance = self.distance(r, c)
+        self.r = r
+        self.c = c
+        return distance
+
+
+    def distance(self, r, c):
+        return math.ceil(num.sqrt((self.r-r)**2 + (self.c-c)**2))
