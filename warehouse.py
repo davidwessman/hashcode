70e2b54217e3762394344508154f8c0d2fa68ee3
philipedenborg <philip.edenborg@gmail.com>
2016-02-11 21:13:33 +0100

    Added id and changed deposit and withdraw

--- warehouse.py
+++ warehouse.py
@@ -1,18 +1,23 @@
-class WareHouse:
 
+from operator import add
+class WareHouse:
+    id = 0
     def __init__(self, line1, line2):
+        self.id = WareHouse.id
         self.r = int(line1[0])
         self.c = int(line1[2])
         self.items = map(int, line2.split( ))
+        WareHouse.id += 1
 
 
 
+    def deposit(self, items):
+        for x in xrange(0,len(items)):
+            self.items[x] += items[x]
 
-    def deposit(self, type, nbrOfItems):
-        self.items[type] += nbrOfItems
-
-    def withdraw(self, type, nbrOfItems):
-        self.items[type] -= nbrOfItems
+    def withdraw(self, items):
+        for x in xrange(0,len(items)):
+            self.items[x] -= items[x]
 
     def amountOfType(self, type):
         return self.items[type]
